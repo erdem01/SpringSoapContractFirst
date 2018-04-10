fc188ceebdafdcc55fda70ebb4c2dc7c2ee032ea
Erdem Caglayan <erdemcaglayan_1@hotmail.com>
2018-04-11 02:12:52 +0300

    Created a project to test spring capability on contract first soap service creation.

---
+++ SpringSoapContractFirstDeneme/src/main/java/erdems/spring/soap/deneme/SpringSoapContractFirstDenemeApplication.java
@@ -0,0 +1,15 @@
+package erdems.spring.soap.deneme;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringSoapContractFirstDenemeApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapContractFirstDenemeApplication.class, args);
+	}
+    
+}
